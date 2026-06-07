4974cac81ebecb68737e1925bd431bc9c2548807
moriyatakashi <36041907+moriyatakashi@users.noreply.github.com>
2026-06-07 14:26:17 +0900

    Extract firebase.js

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCtpHqAY9oXWdQdr-DaGFnJWmIfpHUy0ZA",
+  authDomain: "roreki.firebaseapp.com",
+  projectId: "roreki",
+  storageBucket: "roreki.firebasestorage.app",
+  messagingSenderId: "627587600943",
+  appId: "1:627587600943:web:9d895c71e14eeaa2502cda"
+};
+
+export const app = initializeApp(firebaseConfig);
